faadf8c88b1d7266efe50b5ac1706c247fc39d36
Dexter Industries <dxtrindustries@gmail.com>
2015-02-10 11:21:10 -0500

    Adding the web code for lionel train control
    Example web code for lionel train code with the Raspberry Pi.

---
+++ Projects/Lionel_Train_Control/Code/var/www/index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+
+<html><body><h1>Welcom to the Lionel Train Station!</h1>
+
+<img src="train_pic_small.jpg">
+<BR>
+<BR>
+Test the trains!<BR><BR>
+
+<table style="width:100%"><tr>
+<form action="left_switch.php" method="post" value="LEFT">
+<input type="submit" value="Left!">
+</form>
+</tr>
+<tr>
+<form action="right_switch.php" method="post">
+<input type="submit" value="Right!">
+</form>
+</tr>
+</table>
+
+
+</body>
+</html>
+
+
+
+</p>
+</body></html>
---
+++ Projects/Lionel_Train_Control/Code/var/www/left_switch.php
@@ -0,0 +1,29 @@
+<?php    
+
+$file = fopen("test.txt","w") or die("Unable to open file!");
+echo fwrite($file,"0");
+fclose($file);
+
+
+header('Location: index.php');    
+?>
+
+<html>
+<body>
+
+<?php
+$file = fopen("test.txt","w") or die("Unable to open file!");
+echo fwrite($file,"0");
+fclose($file);
+?>
+
+<form action="left_switch.php" method="post">
+<input type="submit">
+</form>
+<form action="right_switch.php" method="post">
+<input type="submit">
+</form>
+
+
+</body>
+</html>
