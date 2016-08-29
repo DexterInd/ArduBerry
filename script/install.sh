3f10840d2d121d9089ea14314bc5bca607cf8556
Shoban94 <shobannarayan@gmail.com>
2016-08-29 21:55:53 +0530

    Change

--- script/install.sh
+++ script/install.sh
@@ -130,87 +130,89 @@ cd /tmp
 
 #cd /tmp
 
-# doBackup() {
-  # cd $1
-  # echo -n " $2: "
-  # if [ -f $2.bak ]; then
-    # echo "Backup of $2 exists, not overwriting"
-  # else
-    # mv $2 $2.bak
-    # mv /tmp/$2 .
-    # echo "OK"
-  # fi
-# }
+doBackup() {
+  cd $1
+  echo -n " $2: "
+  if [ -f $2.bak ]; then
+    echo "Backup of $2 exists, not overwriting"
+  else
+    mv $2 $2.bak
+    mv /tmp/$2 .
+    echo "OK"
+  fi
+}
 
-# echo "Setting up Raspberry Pi to make it work with the Gertboard"
-# echo "and the ATmega chip on-board with the Arduino IDE."
-# echo ""
-# echo "Checking ..."
+echo "Setting up Raspberry Pi to make it work with the Gertboard"
+echo "and the ATmega chip on-board with the Arduino IDE."
+echo ""
+echo "Checking ..."
 
-# echo -n "  Avrdude: "
-# if [ ! -f /usr/share/arduino/hardware/tools/avr/etc/avrdude.conf ]; then
-  # echo "Not installed. Please install it first"
-  # exit 1
-# fi
+echo -n "  Avrdude: "
+if [ ! -f /usr/share/arduino/hardware/tools/avr/etc/avrdude.conf ]; then
+  echo "Not installed. Please install it first"
+  exit 1
+fi
 
-# fgrep -sq GPIO /usr/share/arduino/hardware/tools/avr/etc/avrdude.conf
-# if [ $? != 0 ]; then
-  # echo "No GPIO support. Please make sure you install the right version"
-  # exit 1
-# fi
-# echo "OK"
+fgrep -sq GPIO /usr/share/arduino/hardware/tools/avr/etc/avrdude.conf
+if [ $? != 0 ]; then
+  echo "No GPIO support. Please make sure you install the right version"
+  exit 1
+fi
+echo "OK"
 
-# echo -n "  Arduino IDE: "
-# if [ ! -f /usr/share/arduino/hardware/arduino/avr/programmers.txt ]; then
-  # echo "Not installed. Please install it first"
-  # exit 1
-# fi
-# echo "OK"
+echo -n "  Arduino IDE: "
+if [ ! -f /usr/share/arduino/hardware/arduino/avr/programmers.txt ]; then
+  echo "Not installed. Please install it first"
+  exit 1
+fi
+echo "OK"
 
-# echo "Fetching files:"
-# for file in boards.txt programmers.txt avrsetup ; do
-  # echo "  $file"
-  # rm -f $file
-  # wget -q http://project-downloads.drogon.net/gertboard/$file
-# done
+echo "Fetching files:"
+for file in boards.txt programmers.txt avrsetup ; do
+  echo "  $file"
+  rm -f $file
+  wget -q http://project-downloads.drogon.net/gertboard/$file
+done
 
-# echo "Replacing/updating files:"
+echo "Replacing/updating files:"
 
-# rm -f /usr/local/bin/avrsetup
-# mv /tmp/avrsetup /usr/local/bin
-# chmod 755 /usr/local/bin/avrsetup
+rm -f /usr/local/bin/avrsetup
+mv /tmp/avrsetup /usr/local/bin
+chmod 755 /usr/local/bin/avrsetup
 
-# cd /etc
-# echo -n "inittab: "
-# if [ -f inittab.bak ]; then
-  # echo "Backup exists: not overwriting"
-# else
-  # cp -a inittab inittab.bak
-  # sed -e 's/^.*AMA0.*$/#\0/' < inittab > /tmp/inittab.$$
-  # mv /tmp/inittab.$$ inittab
-  # echo "OK"
-# fi
+cd /etc
+echo -n "inittab: "
+if [ -f inittab.bak ]; then
+  echo "Backup exists: not overwriting"
+else
+  cp -a inittab inittab.bak
+  sed -e 's/^.*AMA0.*$/#\0/' < inittab > /tmp/inittab.$$
+  mv /tmp/inittab.$$ inittab
+  echo "OK"
+fi
 
-# cd /boot
-# echo -n "cmdline.txt: "
-# if [ -f cmdline.txt.bak ]; then
-  # echo "Backup exists: not overwriting"
-# else
-  # cp -a cmdline.txt cmdline.txt.bak
-  # cat cmdline.txt					|	\
-		# sed -e 's/console=ttyAMA0,115200//'	|	\
-		# sed -e 's/console=tty1//'		|	\
-		# sed -e 's/kgdboc=ttyAMA0,115200//' > /tmp/cmdline.txt.$$
-  # mv /tmp/cmdline.txt.$$ cmdline.txt
-  # echo "OK"
-# fi
+cd /boot
+echo -n "cmdline.txt: "
+if [ -f cmdline.txt.bak ]; then
+  echo "Backup exists: not overwriting"
+else
+  cp -a cmdline.txt cmdline.txt.bak
+  cat cmdline.txt					|	\
+		sed -e 's/console=ttyAMA0,115200//'	|	\
+		sed -e 's/console=tty1//'		|	\
+		sed -e 's/kgdboc=ttyAMA0,115200//' > /tmp/cmdline.txt.$$
+  mv /tmp/cmdline.txt.$$ cmdline.txt
+  echo "OK"
+fi
 
-# doBackup /usr/share/arduino/hardware/arduino/avr boards.txt
-# doBackup /usr/share/arduino/hardware/arduino/avr programmers.txt
+#doBackup /usr/share/arduino/hardware/arduino/avr boards.txt
+#doBackup /usr/share/arduino/hardware/arduino/avr programmers.txt
+sudo rm /usr/share/arduino/hardware/arduino/avr/programmers.txt
+sudo cp /home/pi/ArduBerry/script/programmers.txt /usr/share/arduino/hardware/arduino/avr/programmers.txt
 
-# echo "All Done."
-# echo "Check and reboot now to apply changes."
-# exit 0
+echo "All Done."
+echo "Check and reboot now to apply changes."
+exit 0
 ##******Setup.sh Ends***********##
 #chmod +x setup.sh
 #sudo ./setup.sh
