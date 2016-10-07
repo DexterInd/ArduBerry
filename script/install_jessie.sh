8b0052abb86b5a2ef138ca08a472f02aeda64ff4
Shoban94 <shobannarayan@gmail.com>
2016-10-07 20:23:49 +0530

    ArduBerry Banner and a missing if in install_jessie.sh

--- script/install_jessie.sh
+++ script/install_jessie.sh
@@ -44,7 +44,8 @@ then
 	echo " "
 	echo "Check for internet connectivity..."
 	echo "=================================="
-	wget -q --tries=2 --timeout=20 --output-document=/dev/null http://raspberrypi.org 
+	wget -q --tries=2 --timeout=20 --output-document=/dev/null https://raspberrypi.org 
+	if [ $? -eq 0 ];then
 		echo "Connected"
 	else
 		echo "Unable to Connect, try again !!!"
